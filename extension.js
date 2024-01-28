57435986803d4a80f19d485472212a6992f09bf4
Alex Buglak <alexeybuglak24@gmail.com>
2024-01-28 15:58:23 +0100

    updated scripts & went linter

--- extension.js
+++ extension.js
@@ -1,22 +1,14 @@
 const vscode = require('vscode');
 const Client = require('./lib/client.js');
 
-
 function activate(context) {
-  console.log('Congratulations, your extension "htmlclasses-parser" is now active!');
+  const window = vscode.window;
+  const client = new Client(window);
 
-  const disposable = vscode.commands.registerCommand('htmlclasses-parser.helloWorld', () => {
-    vscode.window.showInformationMessage('Hello World from htmlClasses-parser!');
-  });
-
-  const test = vscode.commands.registerCommand('htmlclasses-parser.test', () => {
-    const window = vscode.window;
-    const client = new Client(window);
-
+  const run = vscode.commands.registerCommand('htmlclasses-parser.run', () => {
     client.convert();
-    // client.print(classes.toString());
   });
-  context.subscriptions.push(disposable, test);
+  context.subscriptions.push(run);
 }
 
 // This method is called when your extension is deactivated
@@ -24,5 +16,5 @@ function deactivate() {}
 
 module.exports = {
   activate,
-  deactivate
+  deactivate,
 };
